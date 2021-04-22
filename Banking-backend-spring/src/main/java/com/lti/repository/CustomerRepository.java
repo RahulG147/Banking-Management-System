20fdc580ea517ed2fc483103eb9d923221dd1419
PoojaPreethaJ <jpoojaofficial@gmail.com>
2021-04-22 15:25:36 +0530

    general detail ka addition to the updateCredential code

--- Banking-backend-spring/src/main/java/com/lti/repository/CustomerRepository.java
+++ Banking-backend-spring/src/main/java/com/lti/repository/CustomerRepository.java
@@ -79,6 +79,8 @@ public class CustomerRepository  extends GenericRepository{
 			.executeUpdate();
 		}
 		
+		
+		
 		public boolean isCustomerIdPresent(long customerId) {
 			return (Long)
 					entityManager
